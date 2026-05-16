0a67f8b99e3a6068fcf3b58bbf7b6bb296d06b6a
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-05-16 13:00:41 +0200

    ✨: add 3D model of a MacBook and enhance scene with environment and background color

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,14 +1,18 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, Float, OrbitControls, useGLTF } from "@react-three/drei";
 
 export function Experience() {
+  const computer = useGLTF(
+    "https://threejs-journey.com/resources/models/macbook_model.gltf",
+  );
   return (
     <>
+      <color attach="background" args={["#241a1a"]} />
+      <Environment preset="city" />
       <OrbitControls makeDefault />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Float rotationIntensity={0.4}>
+        <primitive object={computer.scene} position-y={-1.2} />
+      </Float>
     </>
   );
 }
